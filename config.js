3e28c883e310a99dfdcaf2ebaf8f29a86c8c9b2d
FelpzSystem <soulvsoulv2@gmail.com>
2026-04-27 23:49:05 -0300

    primeiro commit

---
+++ config.js
@@ -0,0 +1,47 @@
+
+/* @CLOVERMYT */
+
+// Canal: https://youtube.com/@clovermyt
+
+// Canal WhatsApp: https://whatsapp.com/channel/0029Va974hY2975B61INGX3Q
+
+// Instagram: @r4mon_sant0s
+
+// Comunidade WhatsApp: https://chat.whatsapp.com/Kc5HLGCIokb37mA36NJrM6
+
+// SE FOR REPOSTAR ME MARCA 🧙‍♂️🍀
+
+
+
+// token de seu Cluster do mongoDb(deixei esse padrao pra vcs, mas recomendo mudar isso)
+
+// foto inicial  do usuario
+const ft = "https://i.postimg.cc/QtLVtyht/ef984fd8e2ab3148a1470c4fa3586475.jpg";
+
+// saldo inicial do novo usuario
+const saldo = 50;
+
+// total de saldo que ja foi usado pelo novo usuario
+const total = 0;
+
+// nome padrao do insta do novo usuario (me segue no insta 💕)
+const insta = "@seu_insta"
+
+// zap padrao do novo usuario (numero que ta aqui nao existe kkk)
+const zap = "5598981564948"
+
+// youtube ppadrao do novo usuario (se baixou entao ja segue ne?)
+const yt = "youtube.com/@seu_nick"
+
+// wallpaper padrao tbm
+const wallpaper = "https://i.postimg.cc/YqWwbCHM/08f74e88a3d8073cc58c79c61e6734ce.jpg"
+
+module.exports = {
+    ft,
+    total,
+    insta,
+    zap,
+    yt,
+    wallpaper,
+    saldo
+}
